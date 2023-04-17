d6b8543cc4835f10f28a4e01b6d3cff4f2f59ead
Saana Kanniainen <saana.m.kanniainen@student.oulu.fi>
2023-04-17 22:28:06 +0300

    added screenshot that was lost in merge

--- src/main/java/com/ohj4/MyButtonActions.java
+++ src/main/java/com/ohj4/MyButtonActions.java
@@ -1,7 +1,14 @@
 package com.ohj4;
 
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 /**
  * The class MyButtonActions implements ActionListener and defines actions for different button
@@ -23,6 +30,15 @@ public class MyButtonActions implements ActionListener {
         } else if (command == "screenshot") {
             // 'go rank' pressed
             System.out.println(command + " pressed.");
+            BufferedImage image;
+                    try {
+                        image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                        ImageIO.write(image, "png", new File("C:/Users/sanpi/Lipasto/Ohjelmointi 4/tierapp/screenshot.png"));
+                        //TODO: Fix the path and take screenshot only from the application, not the whole screen
+                    } catch (Exception e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
         } else {
             throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
         }                
