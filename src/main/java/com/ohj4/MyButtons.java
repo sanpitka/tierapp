2bbed82d27d5c4ad18bad6b39d300572d4e689b4
Saana Kanniainen <saana.m.kanniainen@student.oulu.fi>
2023-04-18 21:47:46 +0300

    added sidebar menu

--- src/main/java/com/ohj4/MyButtons.java
+++ src/main/java/com/ohj4/MyButtons.java
@@ -3,7 +3,9 @@ package com.ohj4;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 /**
  * The MyButtons class creates a JButton with customized properties and adds an ActionListener to it.
@@ -21,8 +23,21 @@ public class MyButtons extends JPanel {
         }
         newButton.setFocusPainted(false);
         newButton.addActionListener(new MyButtonActions());
-        newButton.setVisible(true);
 
+        return newButton;
+    }
+
+    public JMenuItem setMenuButton(String buttonText, String buttonAction) {
+
+        JMenuItem newButton = new JMenuItem(buttonText);
+        newButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        newButton.setBackground(Color.BLACK);
+        newButton.setBorderPainted(false);
+        newButton.setBorder(null);
+        newButton.setActionCommand(buttonAction);
+        newButton.setForeground(Color.WHITE);
+        newButton.addActionListener(new MyButtonActions());
+        
         return newButton;
     }
 
